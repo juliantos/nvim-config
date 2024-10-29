981b350cc78568ef85778ad8ae5d68f209864b0b
Julian Santos <julian.f.santos88@gmail.com>
2024-10-29 16:30:20 -0400

    theme update

--- lua/juliantos/init.lua
+++ lua/juliantos/init.lua
@@ -3,5 +3,5 @@ require("juliantos.lazy")
 require("juliantos.remaps")
 require("juliantos.sets")
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme 'catppuccin-macchiato'
 vim.o.termguicolors = true
